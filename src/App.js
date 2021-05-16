dac28db7900ec69839b85f76baf05ad209c333f1
Joseph silvester <josi16ee@cmrit.ac.in>
2021-05-17 00:21:55 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Login from "./components/Login";
+import Registration from "./components/Registration";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Welcome from "./components/Welcome";
+import { Typography } from "@material-ui/core";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Typography>
+          <div className="App">
+            <Switch>
+              <Route path="/" exact component={Registration} />
+              <Route path="/Login" component={Login} />
+              <Route path="/Welcome" component={Welcome} />
+            </Switch>
+          </div>
+        </Typography>
+      </Router>
+    );
+  }
 }
 
 export default App;
